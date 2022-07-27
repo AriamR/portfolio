fdc5337b16a08b918fc401399f74451a77c71dca
AriamR <90359429+AriamR@users.noreply.github.com>
2022-07-27 10:35:25 -0500

    make it better

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./nav.css"
-import {ImHome} from "react-icons/im"
+import {FaHome} from "react-icons/fa"
 import {FaUserTie} from "react-icons/fa"
 import {MdOutlineComputer} from "react-icons/md"
 import {RiCodeBoxLine} from "react-icons/ri"
@@ -12,7 +12,7 @@ const Nav = () => {
 
   return (
     <nav>
-      <a href="#" className="active"><ImHome/></a>
+      <a href="#" className="active"><FaHome/></a>
       <a href="#about"><FaUserTie/></a>
       <a href="#skill"><MdOutlineComputer/></a>
       <a href="#project"><RiCodeBoxLine/></a>
